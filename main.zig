b4067a2a88bdc2af1bd8702aba94c4f85fd8423e
abvee <icaliberdev@gmail.com>
2024-10-07 01:16:19 +0530

    Camera and relative movement added

--- main.zig
+++ main.zig
@@ -13,16 +13,22 @@ const screen_height = 1080;
 
 pub fn main() void {
 
+	rl.InitWindow(screen_width, screen_height, "skrr");
+	defer rl.CloseWindow();
+
 	// Main player
 	var player: rl.Rectangle = rl.Rectangle{
-		.x = screen_width / 2 - tile_side / 2,
-		.y = screen_height / 2 - tile_side / 2,
+		.x = 0 - tile_side / 2,
+		.y = 0 - tile_side / 2,
 		.width = tile_side,
 		.height = tile_side,
 	};
 
-	rl.InitWindow(screen_width, screen_height, "skrr");
-	defer rl.CloseWindow();
+	// Camera
+	var camera: rl.Camera2D = undefined;
+	camera.offset = rl.Vector2{.x = screen_width / 2, .y = screen_height / 2};
+	camera.rotation = 0;
+	camera.zoom = 1.0;
 
 	while (!rl.WindowShouldClose()) {
 		// input. Only 1 key a frame for now
@@ -35,11 +41,21 @@ pub fn main() void {
 		else if (rl.IsKeyDown(rl.KEY_D))
 			player.x += speed;
 
+		camera.target = rl.Vector2{.x = player.x, .y = player.y};
+
 		// Draw
 		rl.BeginDrawing();
 		defer rl.EndDrawing();
 
 		rl.ClearBackground(rl.BLACK);
+
+		// Player camera
+		rl.BeginMode2D(camera);
+		defer rl.EndMode2D();
+		// reference rectangles
+		rl.DrawRectangle(0, 0, tile_side, tile_side, rl.LIGHTGRAY);
+		rl.DrawRectangle(0 + 2 * tile_side, 0, tile_side, tile_side, rl.LIGHTGRAY);
+
 		rl.DrawRectangleRec(player, rl.RED);
 	}
 }
