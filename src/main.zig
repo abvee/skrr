e45730fb2b9390fb5f2b1d056d6b651694d58fed
abvee <icaliberdev@gmail.com>
2025-06-10 20:56:46 +0530

    Camera

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,7 @@ const window_height = 900;
 const TILE = 120;
 
 // all coordinates are in world space
+// center of the player rectangle
 var player_pos: rl.Vector2 = rl.Vector2{.x = 0, .y = 0};
 var player: rl.Rectangle = rl.Rectangle{
 	.x = 0 - TILE / 2,
@@ -27,11 +28,21 @@ pub fn main() void {
 	rl.InitWindow(window_width, window_height, "skrr");
 	defer rl.CloseWindow();
 
+	const camera: rl.Camera2D = rl.Camera2D{
+		.target = player_pos,
+		.offset = rl.Vector2{.x = window_width/2, .y = window_height/2},
+		.rotation = 0,
+		.zoom = 1, // TODO: maybe change this for resolution resizing stuff ? 
+	};
+
 	while (!rl.WindowShouldClose()) {
 		rl.BeginDrawing();
 		defer rl.EndDrawing();
 
 		rl.ClearBackground(rl.BLACK);
+
+		rl.BeginMode2D(camera);
+		defer rl.EndMode2D();
 		rl.DrawRectangleRec(player, rl.RED);
 	}
 }
