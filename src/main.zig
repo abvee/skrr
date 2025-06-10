4066f90d4a6fb3025618c77290fe5139716c950c
abvee <icaliberdev@gmail.com>
2025-06-10 21:20:12 +0530

    player movement

--- src/main.zig
+++ src/main.zig
@@ -10,6 +10,7 @@ const window_width = 1440;
 const window_height = 900;
 
 const TILE = 120;
+const SPEED = 0.5;
 
 // all coordinates are in world space
 // center of the player rectangle
@@ -28,7 +29,7 @@ pub fn main() void {
 	rl.InitWindow(window_width, window_height, "skrr");
 	defer rl.CloseWindow();
 
-	const camera: rl.Camera2D = rl.Camera2D{
+	var camera: rl.Camera2D = rl.Camera2D{
 		.target = player_pos,
 		.offset = rl.Vector2{.x = window_width/2, .y = window_height/2},
 		.rotation = 0,
@@ -36,6 +37,20 @@ pub fn main() void {
 	};
 
 	while (!rl.WindowShouldClose()) {
+		if (rl.IsKeyDown(rl.KEY_A))
+			player_pos.x -= SPEED
+		else if (rl.IsKeyDown(rl.KEY_D))
+			player_pos.x += SPEED
+		else if (rl.IsKeyDown(rl.KEY_W))
+			player_pos.y -= SPEED
+		else if (rl.IsKeyDown(rl.KEY_S))
+			player_pos.y += SPEED;
+
+		player.x = player_pos.x;
+		player.y = player_pos.y;
+
+		camera.target = player_pos;
+
 		rl.BeginDrawing();
 		defer rl.EndDrawing();
 
@@ -43,7 +58,11 @@ pub fn main() void {
 
 		rl.BeginMode2D(camera);
 		defer rl.EndMode2D();
+
 		rl.DrawRectangleRec(player, rl.RED);
+
+		// Reference rectangle
+		rl.DrawRectangle(TILE / 2, -TILE / 2, TILE, TILE, rl.RAYWHITE);
 	}
 }
 
