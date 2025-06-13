c90d8492366a685dfa8aa5da019682eff047b655
abvee <icaliberdev@gmail.com>
2025-06-14 00:37:37 +0530

    added start position. load code modified and offset added

--- src/level.zig
+++ src/level.zig
@@ -4,15 +4,25 @@ const rl = @cImport({
 	@cInclude("raymath.h");
 	@cInclude("rlgl.h");
 });
+const assert = std.debug.assert;
 
 const constants = @import("constants.zig");
 const TILE = constants.TILE;
 
-// NOTE that the level files have coordinates in with 1 tile as 1 unit
+var offset: u64 = 0; // offset of the level file after load is called
+// This is housekeeping that is used in start_position and other functions
 
+// NOTE that the level files have coordinates in with 1 tile as 1 unit
 pub fn load(allocator: std.mem.Allocator, path: []const u8) ![]rl.Rectangle {
 	const file = try std.fs.cwd().openFile(path, .{});
+	defer file.close();
 	const reader = file.reader();
+
+	// set file offset to 0. Keeping track of which file the offset is for is
+	// not our concern here
+	offset = 0;
+	defer offset = file.getPos() catch 0;
+	// NOTE: we assert that offset is not 0 later
 
 	// We load the level into this
 	var bigboi: [2048]rl.Rectangle = [_]rl.Rectangle{
@@ -22,13 +32,19 @@ pub fn load(allocator: std.mem.Allocator, path: []const u8) ![]rl.Rectangle {
 
 	// read through the file, and load into rectangles
 	var buf: [1024]u8 = undefined;
-	while (true) {
-		const line = reader.readUntilDelimiter(&buf, '\n')
+
+	var line: []u8 = try reader.readUntilDelimiter(&buf, '\n');
+	// EOF here ^ is breaking
+
+	// what is this abomination
+	while (line.len != 0) :
+	(
+		line = reader.readUntilDelimiter(&buf, '\n')
 			catch |e| switch (e) {
 				error.EndOfStream => break,
 				else => return e,
-			};
-
+			}
+	) {
 		var it = std.mem.tokenizeAny(u8, line, ",");
 
 		// fill the struct with tokenized results
@@ -39,7 +55,9 @@ pub fn load(allocator: std.mem.Allocator, path: []const u8) ![]rl.Rectangle {
 				// we catch 0 because what could possibly go wrong ?
 		}
 		bigboi_index += 1;
+
 	}
+
 
 	const ret = try allocator.alloc(rl.Rectangle, bigboi_index);
 	std.mem.copyForwards(rl.Rectangle, ret, bigboi[0..bigboi_index]);
@@ -59,6 +77,18 @@ test "level loading" {
 	std.debug.print("length: {d}\n", .{level.len});
 	for (level) |l|
 		std.debug.print("x: {d:.0} y: {d:.0} width: {d:.0} height: {d:.0}\n", l);
+	std.debug.print("File Offset: {d}\n", .{offset});
 }
 
-fn start_position() void { }
+// return a start position for the player
+pub fn start_position(path: []const u8) !rl.Vector2 {
+	assert(offset != 0);
+	// the level file is very broken if offset is either 0 or getPos() from
+	// std.fs.File could not be called
+
+	const file = try std.fs.cwd().openFile(path, .{});
+	defer file.close();
+
+	// go to offset
+	file.seekTo(offset);
+}
