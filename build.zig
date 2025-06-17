a1f756b358757ab28c3e520d3a1ca4df608a420b
abvee <icaliberdev@gmail.com>
2025-06-17 20:14:47 +0530

    server running

--- build.zig
+++ build.zig
@@ -81,4 +81,28 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run all unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
     test_step.dependOn(&run_level_unit_tests.step);
+
+	// -- SERVER STUFF BEYOND THIS POINT --
+    const server_mod = b.createModule(.{
+        .root_source_file = b.path("server/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const server_exe = b.addExecutable(.{
+        .name = "skrr-server",
+        .root_module = server_mod,
+    });
+	b.installArtifact(server_exe);
+	// server run step
+	const server_run = b.addRunArtifact(server_exe);
+    server_run.step.dependOn(b.getInstallStep());
+
+	// argument passing
+    if (b.args) |args| {
+        server_run.addArgs(args);
+    }
+
+	// run step
+    const server_run_step = b.step("serve", "Start the server");
+    server_run_step.dependOn(&server_run.step);
 }
